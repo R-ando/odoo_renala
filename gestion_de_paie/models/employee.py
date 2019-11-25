f320a66a7c865ad0b70ef3aa7f1a7a91f8f801f5
fano2 <mezanaf@gmail.com>
2019-11-25 03:26:04 +0300

    calcule encienté pour chaque employé

--- gestion_de_paie/models/employee.py
+++ gestion_de_paie/models/employee.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import date, datetime
 
 
 class Employee(models.Model):
@@ -15,4 +16,28 @@ class Employee(models.Model):
     lieu_cin = fields.Char(string='Lieu de délivrance CIN')
     num_emp = fields.Char(string="N° Matricule")
     nombre_enfant_cnaps = fields.Integer(string=u"Nombre d'enfant allouée CNaPS")
+    seniority = fields.Char(string=u'Encieneté', compute='get_seniority')
 
+    @api.multi
+    def get_seniority(self):
+        for employee in self:
+            date_start = self.env['hr.contract'].search([('employee_id', '=', employee.id)]).mapped('date_start')[0].split('-')
+            if date_start:
+                dif_m = self.diff_month(date.today(), datetime(int(date_start[0]), int(date_start[1]), int(date_start[2])))
+                if dif_m < 13:
+                    if dif_m == 12:
+                        employee.seniority = '1 ans'
+                    if dif_m == 0:
+                        d0 = date(int(date_start[0]), int(date_start[1]), int(date_start[2]))
+                        employee.seniority = str(date.today() - d0) + 'jours'
+                    else:
+                        employee.seniority = str(dif_m) + 'mois'
+                else:
+                    mois = dif_m % 12
+                    ans = (dif_m - mois) / 12
+                    employee.seniority = str(ans) + 'ans/' + str(mois) + 'mois'
+            else:
+                employee.seniority = '0'
+
+    def diff_month(self, d1, d2):
+        return (d1.year - d2.year) * 12 + d1.month - d2.month
