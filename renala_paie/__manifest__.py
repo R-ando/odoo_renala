a02cb7cf00022919cf5fd070dfb17808256c06b5
m0r7y <fraharijaona@ingenosya.mg>
2020-02-11 15:05:35 +0000

    [ready]

---
+++ renala_paie/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "renala_paie",
+
+    'summary': """
+        Personnalisation Paie pour RENALA""",
+
+    'description': """
+        Personnalisation Paie pour RENALA
+    """,
+
+    'author': "Rado - Ingenosya",
+    'website': "https://ingenosya.com/",
+
+    'category': 'Human Resources',
+    'version': '1.0',
+
+    'depends': ['base', 'gestion_de_paie'],
+
+    'data': [
+    ],
+
+    'sequence': 1,
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
