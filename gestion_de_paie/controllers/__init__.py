d71bf982a7548874ec9f6cfca652d37866b16d03
m0r7y <fraharijaona@ingenosya.mg>
2020-02-18 10:48:55 +0000

    [FIX] controller

--- gestion_de_paie/controllers/__init__.py
+++ gestion_de_paie/controllers/__init__.py
@@ -1,4 +1,4 @@
 from . import cnaps_controllers
 from . import ostie_contollers
 from . import irsa_controller_bis
-from . import ostie_etat_general_controller
+# from . import ostie_etat_general_controller
